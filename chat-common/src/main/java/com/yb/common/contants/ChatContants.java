70a07ece3d3894d79c3f383029fceb5a8c406794
杨波 <yang.bo@mobcb.com>
2018-03-05 10:52:29 +0800

    上线下线记录保存

---
+++ chat-common/src/main/java/com/yb/common/contants/ChatContants.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2014-2018  墨博云舟 All Rights Reserved.
+ */
+package com.yb.common.contants;
+
+/**
+ * ChatContants:
+ *
+ * @author yangbo
+ * @version 1.00
+ * @since 2018/2/22 0022 17:08
+ */
+public class ChatContants {
+    //登陆记录
+    public static final int LOG_LOGIN = 1;
+    //登出记录
+    public static final int LOG_LOGOUT = 0;
+}
